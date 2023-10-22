7a54e2e5fcf09ecf4bf03aa6e6c329d5c1ca45cc
Unicornium <svasiliy1114@gmail.com>
2023-10-22 23:31:18 +0400

    Create SceneSwich.cs

---
+++ Scripts/SceneSwich.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Analytics;
+
+public class SceneSwich : MonoBehaviour {
+
+	public Animator transition;
+
+	public float transitionTime = 1f;
+
+	void OnTriggerEnter2D(Collider2D Other)
+	{
+		//GameDistribution.Instance.ShowAd();
+		Debug.Log("NextLvlInterstetial!");
+		LoadNextLevel();
+	}
+
+	public void LoadNextLevel()
+	{
+		StartCoroutine(loadLevel(SceneManager.GetActiveScene().buildIndex +1));
+		//Progress.Instance.playerInfo.Level = SceneManager.GetActiveScene().buildIndex;
+		//Save Del if problem
+		Progress.Instance.level = SceneManager.GetActiveScene().buildIndex;
+		//PlayerPrefs.Save();
+		Progress.Instance.EndLevel();
+		//
+		GameAnalytics.gameAnalytics.LogEvent("Level Up");
+	}
+
+	IEnumerator loadLevel(int levelIndex)
+	{
+		//Play animation
+		transition.SetTrigger("Start");
+		//Wait
+		yield return new WaitForSeconds(transitionTime);
+		//Load scene
+		SceneManager.LoadScene(levelIndex);
+	}
+}
