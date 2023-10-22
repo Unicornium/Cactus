95e64778bd1ece941793361f604c539a96461705
Unicornium <svasiliy1114@gmail.com>
2023-10-22 23:31:11 +0400

    Create Platform8.cs

---
+++ Scripts/Platform8.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform8 : MonoBehaviour {
+	Animator anim;
+
+	private Rigidbody2D rb;
+
+	[SerializeField]
+	private AudioSource SoundFallPlatform;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator> ();
+	}
+
+	void OnCollisionEnter2D (Collision2D col) {
+		if ( col.gameObject.name.Equals ("Cactus")) 
+	{
+			Invoke ("DropPlatform", 0.5f);
+			Destroy (gameObject, 2f);
+			anim.SetFloat("Fall", 0.5f);
+			SoundFallPlatform.Play();
+		}
+		
+	}
+
+	void DropPlatform(){
+		rb.isKinematic = false;
+
+	}
+}
