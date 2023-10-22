0242476184077a13ba892231ea499d6be9ec3975
Unicornium <svasiliy1114@gmail.com>
2023-10-22 23:31:27 +0400

    Create UseableSwich.cs

---
+++ Scripts/UseableSwich.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseableSwich : Switch {
+
+	[SerializeField]
+	private AudioSource SwSound;
+
+	public void Use(){
+
+		Toggle ();
+
+		SwSound.Play();
+	}
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		
+	}
+}
