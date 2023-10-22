15a0497924724532152fe0c912ea4eb6684789a2
Unicornium <svasiliy1114@gmail.com>
2023-10-22 23:30:34 +0400

    Create CameraFollow2.cs

---
+++ Scripts/CameraFollow2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2 : MonoBehaviour {
+
+	public Transform target;
+
+	public float YMax = 0;
+
+	public float YMin = 0;
+
+	public float XMax = 0;
+
+	public float XMin = 0;
+
+
+	void FixedUpdate () {
+
+
+		transform.position = new Vector3 (Mathf.Clamp(target.position.x, XMin, XMax), Mathf.Clamp(target.position.y, YMin, YMax), transform.position.z);
+
+	}
+}
