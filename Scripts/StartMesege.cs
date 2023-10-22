ddf27a1f2610794bdf367279795c88121e35aa6f
Unicornium <svasiliy1114@gmail.com>
2023-10-22 23:31:22 +0400

    Create StartMesege.cs

---
+++ Scripts/StartMesege.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMesege : MonoBehaviour {
+
+	float currentTime = 0f;
+	float startingTime = 2f;
+
+	[SerializeField]
+	GameObject LvlBignUI;
+
+	void Start () {
+		currentTime = startingTime;
+	}
+	
+
+	void Update () {
+		currentTime -= 1 * Time.deltaTime;
+		if (currentTime <= 0){
+			currentTime = 0;
+			LvlBignUI.gameObject.SetActive (false);
+		}
+	}
+}
