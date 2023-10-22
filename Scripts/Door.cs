37c9b4e3ebae1638ad11f7da10bc747b8cf517bb
Unicornium <svasiliy1114@gmail.com>
2023-10-22 23:30:41 +0400

    Create Door.cs

---
+++ Scripts/Door.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+	public bool IsOpen;
+	public int KnocksToOpen = 1;
+
+	Animator myAnimator;
+	Collider2D myCollider;
+	int knocks;
+
+	[SerializeField]
+	private AudioSource dooropenSound;
+
+	// Use this for initialization
+	void Start () {
+
+		myAnimator = GetComponent<Animator> ();
+		myCollider = GetComponent<Collider2D> ();
+	}
+
+	public void Open(){
+
+		knocks++;
+		if (!IsOpen && knocks >= KnocksToOpen)
+			SetState (true);
+	}
+
+	public void Close(){
+
+		knocks--;
+		if (IsOpen && knocks < KnocksToOpen)
+			SetState (false);
+	}
+
+	public void Toggle(){
+
+		if (IsOpen)
+			Close ();
+		else
+			Open ();
+	}
+
+
+	void SetState (bool open) {
+
+		IsOpen = open;
+		myAnimator.SetBool ("Open", open);
+		myCollider.isTrigger = open;
+		dooropenSound.Play();
+	}
+}
